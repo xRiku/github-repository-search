da314cebcf183140cd97dc8167eb89e48eb8a67b
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-12 23:19:20 -0300

    Feito abstração passando uma função componente para classe

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleInputChange = (e) => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+        <Form onSubmit={() => {}}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disabled>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
