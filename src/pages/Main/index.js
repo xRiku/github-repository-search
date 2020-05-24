fe0db155d0824a9d1796fa059255b09737886de5
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-23 21:45:51 -0300

    Adicionado try & catch que muda a borda do input se repositorio não existir

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -13,6 +13,7 @@ export default class Main extends Component {
     newRepo: '',
     repositories: [],
     loading: false,
+    failed: false,
   };
 
   // Carregar os dados do localStorage
@@ -44,21 +45,30 @@ export default class Main extends Component {
 
     const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`/repos/${newRepo}`);
+    try {
+      const response = await api.get(`/repos/${newRepo}`);
 
-    const data = {
-      name: response.data.full_name,
-    };
+      const data = {
+        name: response.data.full_name,
+      };
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+        failed: false,
+      });
+    } catch (error) {
+      this.setState({
+        failed: true,
+        loading: false,
+        newRepo: '',
+      });
+    }
   };
 
   render() {
-    const { newRepo, loading, repositories } = this.state;
+    const { newRepo, loading, repositories, failed } = this.state;
 
     return (
       <Container>
@@ -66,7 +76,7 @@ export default class Main extends Component {
           <FaGithubAlt />
           Repositórios
         </h1>
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} failed={failed ? 1 : 0}>
           <input
             type="text"
             placeholder="Adicionar repositório"
@@ -74,7 +84,7 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton loading={loading}>
+          <SubmitButton loading={loading ? 1 : 0}>
             {loading ? (
               <FaSpinner color="#FFF" size={14} />
             ) : (
